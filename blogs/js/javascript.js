9b60b7ada44a6e8731f4e01fcc6e69e63ffe8160
Zawar Fazal <141554675+Zawar7@users.noreply.github.com>
2024-07-24 10:40:56 -0700

    intial commit

---
+++ blogs/js/javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".bar-btn").click(function(){
+		$(".nav-list").animate({left:'0px'},300)
+		$(".bar-btn").hide()
+		$(".cross-btn").show()
+	})
+	$(".cross-btn").click(function(){
+		$(".nav-list").animate({left:'-300px'},300)
+		$(".cross-btn").hide()
+		$(".bar-btn").show()
+	})
+	
+})
---
+++ training-courses/video-editing-course/apply-for-video-editing-course.php
@@ -0,0 +1,543 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Apply for Video Editing Course - Dynamic Developers</title>
+    <meta name="description" content="Apply for video editing course. Dynamic Develpers Software House in Pakistan offering best video editing/ motion graphics course.">
+    
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="H.M. Arslan">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="shortcut icon" type="image/ico" href="../../images/DD - Logo Rounded.ico" alt="Software House in Rahim Yar Khan">
+    <style>
+
+/*css for nav bar*/
+.main-bar{
+  display: flex;
+  padding: 0px 50px;
+  height: 70px;
+  width: 100%;
+}
+.logo{
+  line-height: 70px;
+  width: 100px;
+}
+
+.logo img{
+  width: 120px; 
+}
+.nav-list ul{
+  display: flex;
+  margin:  0px 50px; 
+  line-height: 70px;
+}
+.nav-list li{
+  padding: 0px 15px;
+  list-style-type: none;
+  font-size: 20px;
+  /*font-weight: bold;*/
+}
+.nav-list li a{
+  text-decoration: none;
+  color: white;
+}
+.nav-list li a:hover{
+    border-bottom: 4px solid #FF8C00;
+    padding-bottom: 7px;  
+}
+.toggle-btns{
+  margin-left: auto;
+}
+.bar-btn{
+  background: none;
+  border: none;
+  color: white;
+  outline: none;
+  display: none;
+}
+.bar-btn:hover{
+  outline: none;
+}
+.bar-btn i{
+  font-size: 25px;
+  line-height: 70px;
+  color: white; 
+}
+.cross-btn{
+  background: none;
+  border: none;
+  outline: none;
+  display:none;
+  color: white;
+
+}
+.cross-btn:hover{
+  outline: none;
+}
+.cross-btn i{
+  font-size: 25px;
+  line-height: 70px;
+  color: white; 
+}
+@media (max-width: 850px){
+  .main-bar{
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+  .nav-list ul{
+    display: block;
+    line-height: normal;
+  }
+  .nav-list ul li{
+    padding: 15px 0px;
+  }
+  .nav-list{
+    position: absolute;
+    left: -300px;
+    width: 200px;
+    background: #1d2d50;
+    z-index: 100;
+    height: 100vh;
+  }
+  .bar-btn{
+    display: block;
+  }
+}
+            
+            .pg6{
+     border-bottom: 4px solid #ff8c00;
+    padding-bottom: 7px; 
+}
+.main-bar{
+ background-color: #1D2D50;
+}
+.services-gradient{
+  background: -webkit-linear-gradient(#ff8c00, #a43139);
+}
+.services-gradient h1{
+  color: white;
+  text-align: center;
+  padding: 120px 0px;
+    </style>
+
+<!------Righ click dissable-------->
+    <script>
+  window.oncontextmenu = function () {
+        return false;
+      }
+      $(document).keydown(function (event) {
+        if (event.keyCode == 123) {
+          return false;
+        }
+        else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+          return false;
+        }
+      });
+    </script>
+    <!------Right click dissable End------->
+    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NW94V6J');</script>
+<!-- End Google Tag Manager -->
+
+<!------- Google Search Console------->
+<meta name="google-site-verification" content="duN5TkclIV4juSI4_19hEETmXSKPAP3susf7XFUQklc" />
+<!------End Google Search Console------>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N7785E81HX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N7785E81HX');
+</script>
+<!-- End Global site tag (gtag.js) - Google Analytics -->
+
+<!-- Google Adsense Script -------->
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9588110352542484"
+     crossorigin="anonymous"></script>
+
+<!-- End Google Adsense Script -------->
+
+
+</head>
+<body>
+    
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW94V6J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+
+    <!-- section for nav -->
+    <section>
+        <div class="main-bar">
+            <div class="logo">
+                <img src="../../images/logo2.png" alt="Best Software House in Pakistan">
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li><a href="https://www.dynamicdevelopers.com.pk/" class="pg1">Home</a></li>
+                    <li><a href="https://www.dynamicdevelopers.com.pk/about.html" class="pg2">About</a></li>
+                    <li><a href="https://www.dynamicdevelopers.com.pk/it-services" class="pg3">Services</a></li>
+                     <li><a href="https://www.dynamicdevelopers.com.pk/training-courses" class="pg6">Courses</a></li>
+                     <li><a href="https://www.dynamicdevelopers.com.pk/blogs" class="pg7">Blogs</a></li>
+                    <li><a href="https://www.dynamicdevelopers.com.pk/team" class="pg5">Team</a></li>
+                    <li><a href="https://www.dynamicdevelopers.com.pk/contact" class="pg4">Contact</a></li>
+                </ul>
+            </div>
+            <div class="toggle-btns">
+                <button class="bar-btn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                <button class="cross-btn"><i class="fa fa-times" aria-hidden="true"></i></button>
+            </div>
+        </div>
+    </section>
+    <div class="services-gradient">
+      <h1>Apply for Video Editing Courses</h1>
+    </div>
+
+    <div class="abc">
+    <div class="title">Course Registration</div>
+    <div class="content">
+      <form action="" method="post" enctype="multipart/form-data">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">Full Name</span>
+            <input type="text" name="name" placeholder="Enter your name" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Father Name</span>
+            <input type="text" name="f_name" placeholder="Enter your Father's name" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="email" name="email" placeholder="Enter your email" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Phone Number</span>
+            <input type="number" name="phone" placeholder="Enter your number (WhatsApp)" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Date of Birth</span>
+            <input type="date" name="dob" placeholder="Enter your Date of Birth" required>
+          </div>
+          <div class="input-box">
+            <span class="details">CNIC</span>
+            <input type="number" name="cnic" placeholder="Enter your CNIC number" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Institute</span>
+            <input type="text" name="institute" placeholder="school/college/university" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Current Status</span>
+            <select name="status" required>
+                 <option value="none" disabled="">--Current Status--</option>
+                 <option value="Student">Student</option>
+                 <option value="Employed">Employed</option>
+                 <option value="Un-Employed">Un-Employed</option>
+            </select>
+         </div>
+          <div class="input-box">
+            <span class="details">City</span>
+            <input type="text" name="city" placeholder="Enter your city name" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Country</span>
+            <input type="text" name="country" placeholder="Enter your country name" required>
+          </div>
+          
+         <div class="input-box">
+             <span style="font-weight: 500;
+  margin-bottom: 5px; display: block;" class="details">Picture (upload your picture)</span>
+             <input style="padding:6px" name="file" type="file" accept="image/*" name="" required>
+         </div>
+         <div class="input-box">
+            <span class="details">Course</span>
+            <select name="course" style="width: 100%" required>
+                 <option value="Video Editing">Video Editing</option>
+                 <option value="Web Development">Web Development</option>
+                 <option value="Flutter App Development">Flutter App Development</option>
+                 <option value="React App Development">React App Development</option>
+                 <option value="Graphic Designing">Graphic Desiging</option>
+                 
+                 <option value="SEO & Digital Marketing">SEO & Digital Marketing</option>
+                 <option value="Web Content Writing">Web Content Writing</option>
+                 <option value="Microsoft Office Expert">Microsoft Office Expert</option>
+                 <option value="Blockchain Development">Blockchain Development</option>
+                 <option value="Cyber Security">Cyber Security</option>
+                 <option value="Computer Networking">Computer Networking</option>
+            </select>
+         </div>
+         <div>
+            <span style="font-weight: 500;
+  margin-bottom: 5px; display: block;" class="details">Gender</span>
+            <input type="radio" name="gender" id="male" value="Male" required>
+            <label style="margin-right:8px;" for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="Female" required>
+            <label style="margin-right:8px;" for="female">Female</label>
+            <input type="radio" name="gender" id="other" value="Male" required>
+            <label style="margin-right:8px;" for="other">Other</label>
+        </div>
+
+
+        </div>
+        
+      
+        
+        <div class="button">
+         <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Register</button>
+         
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <style type="text/css">
+      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+
+body{
+ font-family: 'Poppins',sans-serif;
+  background: linear-gradient(135deg, #71b7e6, #9b59b6);
+}
+.abc{
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  margin: 6% auto;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.25);
+}
+@media(max-width: 584px){
+ .abc{
+  max-width: 90%;
+}
+}
+.abc .title{
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+}
+.abc .title::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 30px;
+  border-radius: 5px;
+  background: linear-gradient(135deg, #71b7e6, #9b59b6);
+}
+.content form .user-details{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+}
+form .user-details .input-box{
+  margin-bottom: 15px;
+  width: calc(100% / 2 - 20px);
+}
+
+form .input-box span.details{
+  display: block;
+  font-weight: 500;
+  margin-bottom: 5px;
+}
+.user-details .input-box input, select{
+  height: 45px;
+  width: 100%;
+  outline: none;
+  font-size: 16px;
+  border-radius: 5px;
+  padding-left: 15px;
+  border: 1px solid #ccc;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+}
+.user-details .input-box input:focus,
+.user-details .input-box input:valid{
+  border-color: #9b59b6;
+}
+ 
+
+ 
+ form .button{
+   height: 45px;
+   margin: 35px;
+   background: white;
+ }
+ form .button button{
+   height: 100%;
+   width: 100%;
+   border-radius: 5px;
+   border: none;
+   color: #fff;
+   margin: 0px;
+   font-size: 18px;
+   font-weight: 500;
+   letter-spacing: 1px;
+   cursor: pointer;
+   transition: all 0.3s ease;
+   background: linear-gradient(135deg, #71b7e6, #9b59b6);
+ }
+ form .button button:hover{
+  /* transform: scale(0.99); */
+  background: linear-gradient(-135deg, #71b7e6, #9b59b6);
+  }
+ @media(max-width: 584px){
+ .container{
+  max-width: 100%;
+}
+form .user-details .input-box{
+    margin-bottom: 15px;
+    width: 100%;
+  }
+  form .category{
+    width: 100%;
+  }
+  .content form .user-details{
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+  .user-details::-webkit-scrollbar{
+    width: 5px;
+  }
+  }
+  @media(max-width: 459px){
+  .abc .content .category{
+    flex-direction: column;
+  }
+}
+
+  </style>
+    
+
+           <!-- section for footer -->
+          <style>/*----------Footer------------*/
+	/*footer css*/
+	footer{
+		background: #1d2d50;
+		padding: 20px 0px;
+	}
+	.pg-links h2{
+		color: white;
+		/*font-size: 25px;*/
+	}
+	.footer-list{
+	    display:flex;
+	    justify-content:center;
+	    flex-wrap: wrap;
+	}
+	.footer-list li{
+	padding: 25px 10px;
+	list-style-type: none;
+	font-size: 20px;
+	/*font-weight: bold;*/
+}
+.footer-list li a{
+	text-decoration: none;
+	color: white;
+}
+.footer-list li a:hover{
+    border-bottom: 4px solid #FF8C00;
+    padding-bottom: 7px;  
+}
+.social-links {
+	display: flex;
+	justify-content: center;
+	border-bottom:2px solid white;
+	padding-bottom:20px;
+}
+.social-links ul{
+	display: flex;
+}
+.social-links ul li{
+	list-style-type: none;
+	padding: 0px 10px
+}
+.social-links ul li a{
+	text-decoration: none;
+	color: white;
+}
+.social-links ul li a i{
+	font-size: 25px;
+}
+.social-links ul li:hover{
+	transform: scale(1.5);
+	}
+	.copyright-info{
+		text-align: center;
+		margin-top: 30px;
+	}
+	.copyright-info p{
+		color: white;
+    font-size: 20px;
+	}
+	.copyright-info p i{
+		margin-right: 10px;
+	}
+</style>
+          
+<section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class=" col-md-12 col-sm-12">
+          <div class="pg-links">
+              
+              <h2 class="text-center" style="font-size:32px; padding-bottom:5px; border-bottom:2px solid white;">Usefull Links</h2>
+            
+            <ol class="footer-list">
+            <li ><a href="https://www.dynamicdevelopers.com.pk/" class="pg1">Home</a></li>
+            <li><a href="https://www.dynamicdevelopers.com.pk/about.html" class="pg2">About</a></li>
+            <li><a href="https://www.dynamicdevelopers.com.pk/it-services" class="pg3">Services</a></li>
+              <li><a href="https://www.dynamicdevelopers.com.pk/training-courses" class="pg6">Courses</a></li>
+              <li><a href="https://www.dynamicdevelopers.com.pk/blogs" class="pg7">Blogs</a></li>
+            <li><a href="https://www.dynamicdevelopers.com.pk/contact" class="pg4">Contact</a></li>
+            <li><a href="https://www.dynamicdevelopers.com.pk/team" class="pg5">Team</a></li>
+            </ol>
+          </div>
+          <div class="social-links">
+            <ul>
+                    <li><a href="https://web.facebook.com/TheDynamicDevelopers" target="-blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.instagram.com/thedynamicdevelopers/" target="-blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/DynamicDevelop8" target="-blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/dynamic-developers-04442a201/" target="-balnk"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                  </ul>
+          </div>
+          
+          <div class="copyright-info">
+            <p> Copyright <i class="fa fa-copyright" aria-hidden="true"></i>2023 Dynamic Developers | | All Rights Reserved </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</section>
+  <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+<!-- javascrip fle attach -->
+<script src="../../javascript/javascript.js"></script>
+        
+</body>
+</html>
+            
+         
+    
